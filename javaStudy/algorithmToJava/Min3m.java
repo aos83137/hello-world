64aaf35185bdbc32ebda19d4b1074f5812d3db30
aos83137 <39228935+aos83137@users.noreply.github.com>
2018-07-18 19:46:35 +0900

    Max,Min 方法

---
+++ javaStudy/algorithmToJava/Min3m.java
@@ -0,0 +1,13 @@
+class Min3m {
+    static int min3(int a, int b, int c){
+        int min = a;
+        if(b<min) min = b;
+        if(c<min) min = c;
+ 
+
+        return min;
+    }
+    public static void main(String[] args){
+        System.out.print(min3(10,50,600));
+    }
+}
