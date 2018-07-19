122e77e22479c6bfe56d5e3ede27affebb0462d2
aos83137 <39228935+aos83137@users.noreply.github.com>
2018-07-19 13:08:06 +0900

    30P_Q

---
+++ javaStudy/algorithmToJava/Q8.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Q8 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int sum=0;  
+        System.out.print("정수 입력");
+        int n;
+        n = scan.nextInt();
+
+        sum = n*(n+1)/2;  // 가우스 덧셈 이용
+
+        System.out.printf("1~%d 의 합은 : %d", n, sum);
+
+    }
+}
