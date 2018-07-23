f788098db4ded85405f521251e056f04b61a453b
aos83137 <39228935+aos83137@users.noreply.github.com>
2018-07-23 17:16:21 +0900

    P30_Q9 (두정수 사이의 모든 정수의 합)

---
+++ javaStudy/algorithmToJava/P30_Q9.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class P30_Q9{
+    static int sumof(int a, int b){
+        if(a>b){
+            int t = a; a = b; b = t;
+        }
+        
+        int sum =0;
+        for(int i=a;i<=b;i++){
+            sum += i;
+        }
+
+        return sum;
+    }   
+    
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("정수 입력1 :" );
+        int a = scan.nextInt();
+        System.out.print("정수 입력2 :" );
+        int b = scan.nextInt();
+
+        int sum = sumof(a,b);
+        System.out.printf("%2d~%2d 사이의 정수합은 %d",a,b,sum);
+    }
+}
