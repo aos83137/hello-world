8e0b8d48eda403b8323e6fbe8bca2b194a261401
aos83137 <39228935+aos83137@users.noreply.github.com>
2018-07-25 16:14:57 +0900

    입력값은 버퍼에 남는다 초기화가 필요할땐 해주자 오류남

--- school/classTest2.java
+++ school/classTest2.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
-class person{  //변수 함수 두가지만 와야한다
+class Person{  //변수 함수 두가지만 와야한다
     private String name;    //맴버변수 //private 때문에 p.name쓰면 오류나
     private int age;                //그래서 getName이란 함수를 만들어서 호출한다
     private String phone;  
     private boolean sei;
     //초기화시 문자열은 null, boolean false, int 0
-    person(){}
-    person(String name, int age, String phone, boolean sei){
+    Person(){}
+    Person(String name, int age, String phone, boolean sei){
         this.name = name;
         this.age = age;
         this.phone = phone;
         this.sei = sei;
     }
-    person(String name, String phone){
+    Person(String name, String phone){
         this(name,20,phone,true);
     }
 
@@ -31,12 +31,12 @@ class person{  //변수 함수 두가지만 와야한다
         return sei;
     }
 
-    int setAge(int age){
-        return this.age = age;
+    void setAge(int age){
+        this.age = age;
     }
 
-    boolean setSei(boolean sei){
-        return this.sei = sei;
+    void setSei(boolean sei){
+        this.sei = sei;
     }
 }
 
@@ -44,13 +44,13 @@ class classTest2{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
-        person p = new person("홍길동","010-1234-5678");
+        Person p = new Person("홍길동","010-1234-5678");
         System.out.print("나이 : ");
-        p.setAge(scan.nextInt());
-
-        System.out.print("성별 입력: ");
-        String a = scan.nextLine();
-        if(a == "남자") p.setSei(true);
+        p.setAge(scan.nextInt());      // 입력하면 일단 버퍼에 저장되고 변수에 저장된다??????????????
+                                        // 24엔터키 하면 버퍼에는 24엔터키가 들어감
+        System.out.print("성별 입력: "); // nextInt에 정수만 가져가고 엔터키가 버퍼에 남아있다
+        scan.nextLine(); //버퍼를 비웠다  //그래서 다음 nextLine에서 엔터키가 입력되서 바로 넘어감
+        if( scan.nextLine().charAt(0) == '남' ) p.setSei(true);
         else p.setSei(false);
         
         System.out.println("이름:" + p.getName());  
@@ -59,5 +59,3 @@ class classTest2{
         System.out.println("성:" + ((p.getSei() == true)? "남자": "여자" ));
     }
 }
-
-
