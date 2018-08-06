9ff1563f74ecbb4d3f97d3dc35764a82f8dc20d5
aos83137 <39228935+aos83137@users.noreply.github.com>
2018-08-06 15:05:59 +0900

    Add files via upload

---
+++ school/Person/Person.java
@@ -0,0 +1,18 @@
+package haksa;
+public class Person{ //클래스는 public 과 default 뿐이다.
+    String name;
+    String phone;
+
+    public Person(){
+
+    }
+    public Person(String name, String phone){
+        this.name = name;
+        this.phone = phone;
+    }
+    public void show(){
+        System.out.println("-------------------");
+        System.out.println("이름 : " + name);
+        System.out.println("폰 : " + phone);
+    }
+}
