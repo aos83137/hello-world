68107f857630017c29c8e4e74bd0586f03539206
aos83137 <39228935+aos83137@users.noreply.github.com>
2018-08-08 16:44:29 +0900

    instanceof 사용

--- school/TTTsss2.java
+++ school/TTTsss2.java
@@ -7,8 +7,24 @@ class TTTsss2{
     public static void print(Person p){
         if(p instanceof Student){                               //p 형이 Student인지 확인
             System.out.println("학생입니다.");
+            System.out.println("-------------------");
+            System.out.println("이름 : " + p.getName());
+            System.out.println("폰 : " + p.getPhone());
+
+            Student s = (Student)p;
+            System.out.println("sabun : " + s.hakbun);
+            System.out.println("depart : " + s.grade);
+            System.out.println("salary : " + s.score);
         }else if(p instanceof Teacher){
             System.out.println("선생입니다.");
+            System.out.println("-------------------");
+            System.out.println("이름 : " + p.getName());
+            System.out.println("폰 : " + p.getPhone());
+
+            Teacher t = (Teacher)p;
+            System.out.println("sabun : " + t.getSabun());
+            System.out.println("depart : " + t.getDepart());
+            System.out.println("salary : " + t.getSalary());
         }
 
     }
